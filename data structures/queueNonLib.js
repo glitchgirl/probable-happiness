2aa8437b1e411afdecfc5fe8d4c844c855879f4a
glitchgirl <londonspleen@gmail.com>
2019-11-08 09:49:39 -0500

    formatting and another example

--- data structures/queueNonLib.js
+++ data structures/queueNonLib.js
@@ -1,32 +1,38 @@
 function Queue() {
     this.data = [];
-  }
+}
 
-  Queue.prototype.add = function(record) {
+Queue.prototype.add = function(record) {
     this.data.unshift(record);
-  }
-  
-  Queue.prototype.remove = function() {
+}
+
+Queue.prototype.remove = function() {
     this.data.pop();
-  }
-  
-  Queue.prototype.first = function() {
+}
+
+Queue.prototype.first = function() {
     return this.data[0];
-  }
-  
-  Queue.prototype.last = function() {
+}
+
+Queue.prototype.last = function() {
     return this.data[this.data.length - 1];
-  }
-  
-  Queue.prototype.size = function() {
+}
+
+Queue.prototype.size = function() {
     return this.data.length;
-  }
-  
-  // Create a Queue
-  const q = new Queue();
-  q.add(1);
-  q.add(2);
-  q.add(3);
-  q.remove();
-  console.log(q);
-  console.log(`Size of the Queue: ${q.size()}`)
+}
+
+// Create a Queue
+const q = new Queue();
+q.add(1);
+q.add(2);
+q.add(3);
+q.remove();
+console.log(q);
+console.log(`Size of the Queue: ${q.size()}`)
+
+const z = new Queue();
+z.add(4);
+console.log(z);
+console.log(`${z.size()}`);
+console.log()
