958fd94dc0c4c86e053f34fdc5c31145fc3bf77a
glitchgirl <londonspleen@gmail.com>
2019-12-03 11:39:26 -0500

    design patterns

--- classes.js
+++ classes.js
@@ -14,4 +14,7 @@ class Rectangle {
 }
 
 const square = new Rectangle(10, 10);
+const nonsquare = new Rectangle(10,15);
 console.log(square.area); // 100
+console.log("next");
+console.log(nonsquare.area);
